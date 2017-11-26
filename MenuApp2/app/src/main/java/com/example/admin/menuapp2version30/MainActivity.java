eb670d6b70783a722186db4c016ee3f1bc42a726
ViktorPodolyan <ViktorPodolyan@gihub.com>
2017-11-26 15:10:48 +0300

    VP_Project

---
+++ MenuApp2/app/src/main/java/com/example/admin/menuapp2version30/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.admin.menuapp2version30;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = (TextView) findViewById(R.id.header);
+        switch(id){
+            case R.id.action_settings :
+                headerView.setText("Настройки");
+                return true;
+            case R.id.open_settings:
+                headerView.setText("Открыть");
+                return true;
+            case R.id.save_settings:
+                headerView.setText("Сохранить");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
