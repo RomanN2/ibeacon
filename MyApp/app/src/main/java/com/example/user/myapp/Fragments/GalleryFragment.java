416a28e32610e121823b96186313d0d2f6d7bd50
RomanSoviak <romansoviak@gmail.com>
2018-05-15 23:03:32 +0300

    Remove views. Rename fragment

--- MyApp/app/src/main/java/com/example/user/myapp/Fragments/GalleryFragment.java
+++ MyApp/app/src/main/java/com/example/user/myapp/Fragments/GalleryFragment.java
@@ -1,6 +1,5 @@
 package com.example.user.myapp.Fragments;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -13,12 +12,12 @@ import com.example.user.myapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FragmentGalery.OnFragmentInteractionListener} interface
+ * {@link GalleryFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link FragmentGalery#newInstance} factory method to
+ * Use the {@link GalleryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentGalery extends Fragment {
+public class GalleryFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +29,7 @@ public class FragmentGalery extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public FragmentGalery() {
+    public GalleryFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +39,11 @@ public class FragmentGalery extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentGalery.
+     * @return A new instance of fragment GalleryFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentGalery newInstance(String param1, String param2) {
-        FragmentGalery fragment = new FragmentGalery();
+    public static GalleryFragment newInstance(String param1, String param2) {
+        GalleryFragment fragment = new GalleryFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +64,7 @@ public class FragmentGalery extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_galery, container, false);
+        return inflater.inflate(R.layout.gallery_fragment, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- MyApp/app/src/main/java/com/example/user/myapp/NavigationActivity.java
+++ MyApp/app/src/main/java/com/example/user/myapp/NavigationActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -17,7 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.user.myapp.Fragments.DevTeamFragment;
-import com.example.user.myapp.Fragments.FragmentGalery;
+import com.example.user.myapp.Fragments.GalleryFragment;
 import com.example.user.myapp.Fragments.FragmentImport;
 import com.example.user.myapp.Fragments.FragmentSend;
 import com.example.user.myapp.Fragments.FragmentShare;
@@ -27,13 +28,12 @@ import com.example.user.myapp.Fragments.FragmentTools;
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     FragmentImport mFragmentImport;
-    FragmentGalery mFragmentGalery;
+    GalleryFragment mGalleryFragment;
     FragmentSend mFragmentSend;
     FragmentShare mFragmentShare;
     FragmentTools mFragmentTools;
     DevTeamFragment mDevTeamFragment;
     FragmentSlideshow mFragmentSlideshow;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +64,7 @@ public class NavigationActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        mFragmentGalery = new FragmentGalery();
+        mGalleryFragment = new GalleryFragment();
         mFragmentImport = new FragmentImport();
         mFragmentSend = new FragmentSend();
         mFragmentShare = new FragmentShare();
@@ -86,37 +86,25 @@ public class NavigationActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.navigation_, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-
         if (id == R.id.nav_camera) {
             fragmentTransaction.replace(R.id.container, mFragmentImport);
         } else if (id == R.id.nav_gallery) {
-            fragmentTransaction.replace(R.id.container, mFragmentGalery);
+            fragmentTransaction.replace(R.id.container, mGalleryFragment);
         } else if (id == R.id.nav_slideshow) {
             fragmentTransaction.replace(R.id.container, mFragmentSlideshow);
         } else if (id == R.id.nav_manage) {
